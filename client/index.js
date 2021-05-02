720c241ba9d86e72fcd213bad15872ff157c157a
Andrea Tessari <andrea.tessari.3@studenti.unipd.it>
2021-05-02 16:20:58 +0200

    fix forklift login, GREG CONTROLLA CHE TI VADA BENE

--- client/index.js
+++ client/index.js
@@ -14,7 +14,8 @@ const SERVER_PORT = 1723;
 const io = require("socket.io")(http, {
     cors: {
     //   origin: "http://localhost:4200",
-        origin: `http://localhost:${process.argv[2]}`,
+    //   origin: `http://localhost:${process.argv[2]}`,
+        origin: `${process.argv[2]}`,
         methods: ["GET", "POST"]
     }
 });
@@ -50,7 +51,7 @@ let c = new Container();
 
 var client = net.connect(SERVER_PORT, 'localhost', ()=>{
     console.log('connected to server');
-    client.write("UNIT\n");
+    client.write(`FORKLIFT\n${process.argv[6]}\n${process.argv[7]}`);
     client.setNoDelay();
 });
 client.setEncoding('utf8');
