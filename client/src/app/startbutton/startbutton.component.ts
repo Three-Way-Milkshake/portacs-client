839a420ce8b1fb765a37ad33f923cef998f76e7c
Andrea Tessari <andrea.tessari.3@studenti.unipd.it>
2021-05-10 10:26:51 +0200

    fix start button

--- client/src/app/startbutton/startbutton.component.ts
+++ client/src/app/startbutton/startbutton.component.ts
@@ -1,15 +1,23 @@
 import { StartbuttonService } from './../services/startbutton.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-startbutton',
   templateUrl: './startbutton.component.html',
   styleUrls: ['./startbutton.component.css']
 })
-export class StartButtonComponent {
+export class StartButtonComponent implements OnInit {
   isOnScreen : boolean = true;
 
-  constructor(private service: StartbuttonService) { }
+  constructor(private service: StartbuttonService, private ngZone: NgZone) { }
+
+  ngOnInit(){
+    this.service.onNewStart().subscribe(() => {
+      this.ngZone.run(() => {
+        this.isOnScreen = true;
+      });      
+    });
+  }
 
   start() {
     this.service.onPress();
